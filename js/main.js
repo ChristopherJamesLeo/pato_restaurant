21b1a4f5675785b1d61b4d342af2b4d32d81efd0
ChristopherJamesLeo <aungaungoo.hotmail@gmail.com>
2023-09-06 15:57:43 +0630

    nav bar

--- js/main.js
+++ js/main.js
@@ -1,7 +1,14 @@
 console.log("hello world");
 
-window.onload = function() {
-    Particles.init({
-      selector: '.background'
-    });
-  };
+// start jquery area
+$(document).ready(function(){
+    console.log("helloworld");
+    $(".navbar_toggle").click(function(){
+        $(".navbar_slide_list_group").addClass("active");
+    })
+    $(".close_navbar_slide_btn").click(function(){
+        $(".navbar_slide_list_group").removeClass("active");
+    })
+})
+
+// end jquery area
